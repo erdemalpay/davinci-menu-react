9aaadfb07d7d42038db033aecd4fa2f55decfea4
Erdem Alpay <erdemalpay@gmail.com>
2023-12-03 12:40:53 +0300

    Test relative path for images

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <div className="head-container w-full  flex items-center justify-center gap-3 max-md:h-[120px] h-36">
-      <img src="src/assets/logo.svg" />
+      <img src="../assets/logo.svg" />
       <div className="text-xl font-bold max-sm:text-base">
         DA VINCI BOARD GAME MENU
       </div>
