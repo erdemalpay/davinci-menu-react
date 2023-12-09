fe87664012c506aaf002f0162cda282e501a8280
Erdem Alpay <erdemalpay@gmail.com>
2023-12-10 01:37:06 +0300

    Adjust sizes

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -16,12 +16,12 @@ const CategoryCard = ({ category, isActive }: IProps) => {
       <div
         className={`${
           isActive ? "border-4 border-rose-700 border-solid" : ""
-        } flex justify-center items-center shadow hower:shadow-xl hover:transform hover:scale-105 md:rounded-xl bg-white duration-300 hover:cursor-pointer`}
+        } flex justify-center items-center shadow hower:shadow-xl md:h-36 md:w-36 h-24 w-24 hover:transform hover:scale-105 md:rounded-xl bg-white duration-300 hover:cursor-pointer`}
       >
         <img
           src={imageUrl || `./assets/${getRandom()}.png`}
           alt="card-image"
-          className="h-24 max-md:h-14 object-fit"
+          className="h-24 md:h-36 object-fit"
         />
       </div>
       <p className="text-center mt-1 text-slate-700 max-md:text-sm">{name}</p>
